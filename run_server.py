2bb4ee46eb4dae42ef0823d826559b9bc75af3fb
Qadeem Arif <58244969+iabdulqadeem@users.noreply.github.com>
2026-01-18 15:37:13 +0500

    Update server IP address for local network access

--- run_server.py
+++ run_server.py
@@ -6,7 +6,7 @@ from database import db, login_manager
 
 if __name__ == '__main__':
     print("🚀 Trackly is starting on the local network...")
-    print("Tell your coworkers to visit: http://10.1.0.52:5000") # Put your IP here
+    print("Tell your coworkers to visit: http://0.0.0.0:5000") # Put your IP here
     # host='0.0.0.0' makes it available to everyone on the LAN/Wi-Fi
     with app.app_context():
         #This creates the tables in Postgres based on models.py
